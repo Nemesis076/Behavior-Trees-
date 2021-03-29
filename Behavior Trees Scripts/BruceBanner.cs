78173ca91a09950ab6ab71ecbb08649710992e65
Nemesis076 <68237452+Nemesis076@users.noreply.github.com>
2021-03-28 20:05:56 -0700

    Add files via upload

---
+++ Behavior Trees Scripts/BruceBanner.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BruceBanner : MonoBehaviour
+{
+    public Door theDoor;
+    public GameObject theTreasure;
+    bool executingBehavior = false;
+    Task myCurrentTask;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            if (!executingBehavior)
+            {
+                executingBehavior = true;
+                myCurrentTask = BuildTask_GetTreasue();
+                myCurrentTask.run();
+            }
+        }
+    }
+
+    private void OnTaskFinished()
+    {
+        executingBehavior = false;
+    }
+
+    Task BuildTask_GetTreasue()
+    {
+       
+        List<Task> taskList = new List<Task>();
+
+        Task isDoorNotLocked = new IsFalse(theDoor.isLocked);
+        Task waitABeat = new Wait(0.5f);
+        Task openDoor = new OpenDoor(theDoor);
+        taskList.Add(isDoorNotLocked);
+        taskList.Add(waitABeat);
+        taskList.Add(openDoor);
+        Sequence openUnlockedDoor = new Sequence(taskList);
+
+        taskList = new List<Task>();
+        Task isDoorClosed = new IsTrue(theDoor.isClosed);
+        Task hulkOut = new HulkOut(this.gameObject);
+        Task bargeDoor = new BargeDoor(theDoor.transform.GetChild(0).GetComponent<Rigidbody>());
+        Task moveDoor = new fixDoor(theDoor.transform.GetChild(0).GetComponent<Rigidbody>());
+        taskList.Add(isDoorClosed);
+        taskList.Add(waitABeat);
+        taskList.Add(hulkOut);
+        taskList.Add(waitABeat);
+        taskList.Add(bargeDoor);
+        taskList.Add(waitABeat);
+        taskList.Add(moveDoor);
+        Sequence bargeClosedDoor = new Sequence(taskList);
+
+        // open a closed door, one way or another
+        taskList = new List<Task>();
+        taskList.Add(openUnlockedDoor);
+        taskList.Add(bargeClosedDoor);
+        Selector openTheDoor = new Selector(taskList);
+
+        // get the treasure when the door is closed
+        taskList = new List<Task>();
+        Task moveToDoor = new MoveKinematicToObject(this.GetComponent<Kinematic>(), theDoor.gameObject);
+        Task moveToTreasure = new MoveKinematicToObject(this.GetComponent<Kinematic>(), theTreasure.gameObject);
+        Task yeetTreasure = new throwTreasure(target: theTreasure.gameObject.AddComponent<Rigidbody>());
+        Task stopMovement = new stopAtObject(this.GetComponent<Kinematic>());
+        taskList.Add(moveToDoor);
+        taskList.Add(waitABeat);
+        taskList.Add(openTheDoor); // one way or another
+        taskList.Add(waitABeat);
+        taskList.Add(moveToTreasure);
+        taskList.Add(stopMovement);
+        taskList.Add(yeetTreasure);
+        Sequence getTreasureBehindClosedDoor = new Sequence(taskList);
+
+        // get the treasure when the door is open 
+        taskList = new List<Task>();
+        Task isDoorOpen = new IsFalse(theDoor.isClosed);
+        taskList.Add(isDoorOpen);
+        taskList.Add(moveToTreasure);
+        taskList.Add(stopMovement);
+        taskList.Add(yeetTreasure);
+        Sequence getTreasureBehindOpenDoor = new Sequence(taskList);
+
+        // get the treasure, one way or another
+        taskList = new List<Task>();
+        taskList.Add(getTreasureBehindOpenDoor);
+        taskList.Add(getTreasureBehindClosedDoor);
+        Selector getTreasure = new Selector(taskList);
+
+        return getTreasure;
+    }
+}
---
+++ Behavior Trees Scripts/Task.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Timers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Task
+{
+    public abstract bool run();
+}
+
+public class IsTrue : Task
+{
+    bool varToTest;
+
+    public IsTrue(bool someBool)
+    {
+        varToTest = someBool;
+
+    }
+
+    public override bool run()
+    {
+        return varToTest;
+    }
+}
+
+
+public class IsFalse : Task
+{
+    bool varToTest;
+
+    public IsFalse(bool someBool)
+    {
+        varToTest = someBool;
+    }
+
+    public override bool run()
+    {
+        return !varToTest;
+    }
+}
+
+public class OpenDoor : Task
+{
+    Door mDoor;
+
+    public OpenDoor(Door someDoor)
+    {
+        mDoor = someDoor;
+    }
+
+    public override bool run()
+    {
+        return !mDoor.Open();
+    }
+}
+
+public class BargeDoor : Task
+{
+    Rigidbody mDoor;
+
+    public BargeDoor(Rigidbody someDoor)
+    {
+        mDoor = someDoor;
+    }
+
+    public override bool run()
+    {
+        mDoor.AddForce(-10f, 0, 0, ForceMode.VelocityChange);
+        return true;
+    }
+}
+
+public class HulkOut : Task
+{
+    GameObject mEntity;
+
+    public HulkOut(GameObject someEntity)
+    {
+        mEntity = someEntity;
+    }
+
+    public override bool run()
+    {
+        mEntity.transform.localScale *= 2;
+        mEntity.GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+        return true;
+    }
+}
+
+public class Wait : Task
+{
+    float mTimeToWait;
+
+    public Wait(float time)
+    {
+        mTimeToWait = time;
+    }
+
+    public override bool run()
+    {
+        return true;
+    }
+}
+
+public class MoveKinematicToObject : Task
+{
+    Arriver2 mMover;
+    GameObject mTarget;
+
+    public MoveKinematicToObject(Kinematic mover, GameObject target)
+    {
+        mMover = mover as Arriver2;
+        mTarget = target;
+    }
+
+    public override bool run()
+    {
+        mMover.myTarget= mTarget;
+        return true; 
+    }
+}
+
+public class fixDoor : Task
+{
+    Rigidbody mDoor;
+    public fixDoor(Rigidbody door)
+    {
+        mDoor = door;
+    }
+    public override bool run()
+    {
+        mDoor.AddForce(10f, 0, 0, ForceMode.VelocityChange);
+        mDoor.transform.position = new Vector3(-0.55f, 3f, -5f);
+        mDoor.transform.Rotate(0f, 0f, 0f);
+        return true;
+    }
+}
+
+public class stopAtObject : Task
+{
+    Arriver2 mMover;
+    public stopAtObject(Kinematic mover)
+    {
+        mMover = mover as Arriver2;
+    }
+    public override bool run()
+    {
+        mMover.maxSpeed = 1f;
+        return true;
+    }
+}
+
+public class throwTreasure : Task
+{
+    Rigidbody mTarget;
+    public throwTreasure(Rigidbody target)
+    {
+        mTarget = target;
+    }
+    public override bool run()
+    {
+        mTarget.AddForce(80f, 0, 0, ForceMode.VelocityChange);
+        return true;
+    }
+}
+
+public class Sequence : Task
+{
+    List<Task> children;
+
+
+    public Sequence(List<Task> taskList)
+    {
+        children = taskList;
+    }
+
+    public override bool run()
+    {
+        foreach (Task child in children)
+        {
+            if (child.run() == false)
+                return false;
+        }
+        return true;
+    }
+}
+public class Selector : Task
+    {
+        List<Task> children;
+
+        public Selector(List<Task> taskList)
+        {
+            children = taskList;
+        }
+        public override bool run()
+        {
+            foreach (Task child in children)
+            {
+                if (child.run())
+                    return true;
+            }
+            return false;
+        }
+    }
+
